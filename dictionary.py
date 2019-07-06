cea39e96ce2cc11e51303cf94081d814bbeb1195
Guilherme <gui_aramos@outlook.com>
2019-07-07 08:09:11 +0900

    Adding the final codes

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,8 @@
+# DESCRIPTION #
+# This interactive dictionary works by searching on JSON file.
+# If the word is misspelled the program tries to find a similar word.
+# Created by Guilherme Ramos.
+
 # Libraries
 import json
 import difflib
@@ -12,20 +17,21 @@ def locate (word):
     
     # Assigning  def variables
     word = word.lower() # Str case sensitivity = lowercase
-    sWord = difflib.get_close_matches(word,data)[0] # Geting similiar word
+    sWord = difflib.get_close_matches(word,data)[0] # Geting simiiar word
     answer = None
+    
+    #Messages
     errorMSG = "Not able to find the word or similar one. Please, check it."
+    askMSG = "Did you mean %s instead? Enter Y if yes or N if no: " % (sWord)
 
-    # Checking wether the word exist.
+    # Checking wether the word exist
     if word in data:
-        # Case it exists, returning the definition.
         return data[word]
     
     # Checking wether there are any similar word
     elif len(sWord) > 0:
-        # Confirming the word
         while answer != 'y' and answer != 'n':
-            answer = input("Did you mean %s instead? please, type Y or N: " % sWord)
+            answer = input(askMSG)
             answer = str(answer.lower())
         
         if answer == 'y':
@@ -37,9 +43,19 @@ def locate (word):
     else:
         return errorMSG
     
-### Starting the program ###
+### Start of the program ###
+
 # Asking input word from user
 word = input("Please, inform the word: ")
 
-# Output the results #
-print(locate(word))
+# Assigning the search on the variable
+output = locate(word)
+
+# Output the results split by lines #
+if type(output) == list:
+    for i in output:
+        print(i)
+else:
+    print(output)
+
+### End of the program ###
